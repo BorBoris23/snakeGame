926388fd619ed0375d46439a45df8ee46334ea92
BorBoris23 <zonovboris93@gmail.com>
2021-11-11 11:33:34 +0300

    added playing field

--- myJS.js
+++ myJS.js
@@ -1,10 +1,11 @@
 function createForm()
 {
-    let form = document.createElement('form');
+    let form = document.createElement('div');
     form.setAttribute('name', 'table');
     form.appendChild(createInput('Enter the height of the map', 'height'));
     form.appendChild(createInput('Enter the width of the map', 'width'));
     form.appendChild(createButton());
+    form.classList.add('form');
     document.body.appendChild(form);
 }
 
@@ -12,6 +13,7 @@ function createInput(placeholderMessage, attribute)
 {
     let input = document.createElement('input');
     input.setAttribute('id', attribute)
+    input.required = true;
     input.type = 'number';
     input.placeholder = placeholderMessage;
     return input;
@@ -19,33 +21,63 @@ function createInput(placeholderMessage, attribute)
 
 function createButton()
 {
-    let button = document.createElement('input');
-    button.type = "submit";
-    button.value = "Create field";
+    let button = document.createElement('button');
+    button.innerHtml = 'Create field';
+    button.classList.add('button');
     button.setAttribute('onclick', 'creteField()')
     return button;
 }
 
-function createCell()
+function createCell(x, y)
 {
     let cell = document.createElement('div');
+    cell.setAttribute('positionX', x)
+    cell.setAttribute('positionY', y)
+    cell.classList.add('cell');
+    return cell;
 }
 
 function creteField()
 {
-    let height = document.getElementById('height').value;
-    let width = document.getElementById('width').value;
-    let fieldArea = height * width;
-
+    let height = Number(document.getElementById('height').value);
+    let width = Number(document.getElementById('width').value);
     let field = document.createElement('div');
-    field.setAttribute('height', '50px');
-    field.setAttribute('width', '40px');
-    field.classList.add('cell');
-    // field.setAttribute('height', 'height');
-    // field.setAttribute('width', 'width');
+    let countCell = (height * width) / 10 * 10;
+
+    field.style.width = width * 10 + "px";
+    field.style.height = height * 10 + "px";
+
+    for(let x = 1; x < width; x ++) {
+        for(let y = 1; y < height; y ++) {
+            field.appendChild(createCell(x, y));
+        }
+    }
+    field.classList.add('field');
+    document.body.appendChild(field);
+    let snakeBody = document.querySelector('div[positionx="width/2"][positiony="height/2"]');
+    alert(snakeBody);
 }
 
 createForm();
+
+
+// function assignCoordinatesToCells(width, height)
+// {
+//     let cells = document.getElementsByClassName('cell');
+//
+//     let x = 1,
+//         y = height;
+//     for(let i = 0; i < cells.length; i++) {
+//         if(x > width) {
+//             x = 1;
+//             y--;
+//         }
+//         cells[i].setAttribute('positionX', String(x));
+//         cells[i].setAttribute('positionY', String(y));
+//         x++
+//     }
+// }
+
 
 // let field = document.createElement('div');
 // document.body.appendChild(field);
@@ -80,3 +112,63 @@ createForm();
 
 // createField();
 // createPlayingField();
+
+// function createForm()
+// {
+//     let form = document.createElement('form');
+//     form.setAttribute('name', 'table');
+//     form.appendChild(createInput('Enter the height of the map', 'height'));
+//     form.appendChild(createInput('Enter the width of the map', 'width'));
+//     form.appendChild(createButton());
+//     document.body.appendChild(form);
+// }
+//
+// function createInput(placeholderMessage, attribute)
+// {
+//     let input = document.createElement('input');
+//     input.setAttribute('id', attribute)
+//     input.required = true;
+//     input.type = 'number';
+//     input.placeholder = placeholderMessage;
+//     return input;
+// }
+//
+// function createButton()
+// {
+//     let button = document.createElement('input');
+//     button.type = "submit";
+//     button.value = "Create field";
+//     button.setAttribute('onclick', 'creteField()')
+//     return button;
+// }
+//
+// function createCell(x, y)
+// {
+//     let cell = document.createElement('div');
+//     cell.setAttribute('positionX', x)
+//     cell.setAttribute('positionY', y)
+//     return cell.classList.add('cell');
+// }
+//
+// function creteField()
+// {
+//     let height = Number(document.getElementById('height').value);
+//     let width = Number(document.getElementById('width').value);
+//     let field = document.createElement('div');
+//
+//     for(let x = 1;x <= width; x ++ ) {
+//         for (let y = 1;y <= height; y++ ) {
+//             // field.appendChild(createCell(x, y));
+//             let cell = document.createElement('div');
+//             cell.setAttribute('positionX', String(x));
+//             cell.setAttribute('positionY', String(y));
+//             field.appendChild(cell);
+//         }
+//     }
+//     let a = 123;
+//     alert(a);
+//     field.classList.add('field');
+//     document.body.appendChild(field);
+// }
+//
+// createForm();
